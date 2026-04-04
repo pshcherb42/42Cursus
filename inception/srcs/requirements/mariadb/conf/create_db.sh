a89ffcbb5c77e0bf54c383b7a5506364573597a7
pshcherb42 <pshcherb@student.42barcelona.com>
2026-04-04 14:34:49 +0200

    wordpress no crea la pagina

--- inception/srcs/requirements/mariadb/conf/create_db.sh
+++ inception/srcs/requirements/mariadb/conf/create_db.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-# Start MariaDB service temporarily for initialization
-service mariadb start
-
-# Wait until MariaDB is ready
-until mysqladmin ping --silent; do
-    echo "Waiting for MariaDB to be ready..."
-    sleep 1
-done
-
-# Create database and user from environment variables
-mysql -u root <<EOF
+# Start MariaDB temporarily for initialization
+mysqld --user=mysql --bootstrap << SQLEOF
+USE mysql;
+FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
-EOF
+SQLEOF
 
-# Stop the temporary service and restart mysqld in foreground
-service mariadb stop
+echo "Database initialized. Starting mysqld..."
 
-# Run MariaDB in foreground, binding to all interfaces
-exec mysqld --bind-address=0.0.0.0
+# Run MariaDB in foreground, accessible from other containers
+exec mysqld --user=mysql --bind-address=0.0.0.0
